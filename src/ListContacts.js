5503a7300daa2f1c8de34fd36322b05d28451c55
Trọng Hữu <huulnt@fsoft.com.vn>
2018-07-24 21:05:56 +0700

    first commit

---
+++ src/ListContacts.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class ListContacts extends Component{
+    render(){
+        <ol className='contact-list'>
+
+        </ol>
+    }
+}
+
+export default ListContacts``
