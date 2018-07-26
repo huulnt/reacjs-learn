9462574fef133712eb3c3e09104de506572949ab
Trọng Hữu <huulnt@fsoft.com.vn>
2018-07-26 08:24:39 +0700

    using state

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,11 +1,22 @@
-import React, {Component} from 'react';
-
-class ListContacts extends Component{
-    render(){
+import React from 'react';
+function ListContacts(props){
+    return (
         <ol className='contact-list'>
-
+            {props.contacts.map((contact) => (
+                <li key={contact.id} className='contact-list-item'>
+                    <div className='contact-avatar' style={{
+                        backgroundImage: `url(${contact.avatarURL})`
+                    }}></div>
+                    <div className='contact-detail'>
+                        <p>{contact.name}</p>
+                        <p>{contact.details}</p>
+                    </div>
+                    <button onClick={props.onDeleteContact(contact)} className='contact-remove'>
+                        Remove
+                    </button>
+                </li>
+            ))}
         </ol>
-    }
+    )
 }
-
-export default ListContacts``
+export default ListContacts
